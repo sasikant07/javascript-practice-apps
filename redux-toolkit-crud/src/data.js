c3decfddd459559ef72d356b17fdbacd724aaa8e
Sasikant Biswal <biswalsashikant@gmail.com>
2023-06-25 01:58:10 +0530

    Added redux toolkit crud operation

---
+++ redux-toolkit-crud/src/data.js
@@ -0,0 +1,22 @@
+const userList = [
+    {
+        id: 1,
+        name: "Steve Miller",
+        email: "steve@gmail.com",
+        phone: "1234-567-890"
+    },
+    {
+        id: 2,
+        name: "Ryan Smith",
+        email: "ryan@gmail.com",
+        phone: "1234-567-890"
+    },
+    {
+        id: 3,
+        name: "Nick Taylor",
+        email: "nick@gmail.com",
+        phone: "1234-567-890"
+    }
+]
+
+export default userList;

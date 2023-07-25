bfd079cf8771708da0b902647891693c122072e3
Sasikant Biswal <biswalsashikant@gmail.com>
2023-07-25 16:13:20 +0530

    Added some changes in weekly calender

--- weekly-calender/src/components/WeeklyCalender.js
+++ weekly-calender/src/components/WeeklyCalender.js
@@ -1,15 +1,17 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { addDateBy, areDatesSame, getMonday, range } from "../utils/utils";
 import { DAYS, dummyEvents } from "../constants/consts";
+import Modal from "./Modal ";
 
 
 const HOUR_HEIGHT = 30;
 const HOUR_MARGIN_TOP = 15;
-
+let eventDate;
 const WeeklyCalender = () => {
     const [mondayDate, setMondayDate] = useState(getMonday());
-    const [events, setEvets] = useState(dummyEvents);
+    const [events, setEvents] = useState(dummyEvents);
     const [showModal, setShowModal] = useState(false);
+    const [formData, setFormData] = useState({});
 
     const hourNow = new Date().getHours();
     const minutesNow = new Date().getMinutes();
@@ -22,12 +24,25 @@ const WeeklyCalender = () => {
         setMondayDate(addDateBy(mondayDate, -7));
     }
 
-    const onAddEvent = () => {
+    const onAddEvent = (date) => {
+        const text = prompt("text");
+        const from = prompt("from");
+        const to = prompt("to");
         
+        // setShowModal(true);
+        // const {text, from , to} = formData;
+        eventDate = date;
+        date.setHours(from);
+
+        setEvents((prev) => [...prev, {text, date, howLong: parseInt(to - from)}]);
     }
 
-    const hourLine = hourNow * (HOUR_HEIGHT + HOUR_MARGIN_TOP + HOUR_HEIGHT) / 2 + minutesNow / 2;
+    // const hourLine = hourNow * HOUR_HEIGHT + HOUR_MARGIN_TOP + HOUR_HEIGHT / 2 + minutesNow / 2;
+    const hourLine = hourNow * HOUR_HEIGHT + HOUR_MARGIN_TOP / 8 + minutesNow / 2;
     const eventHours = (HOUR_HEIGHT + HOUR_HEIGHT) / 2;
+
+    useEffect(() => {
+    }, [showModal])
 
   return (
     <>
@@ -57,26 +72,30 @@ const WeeklyCalender = () => {
             </div>
             <div className="hgrid">
                 {DAYS.map((day, index) => (
-                    <div 
-                        key={index} 
-                        className="day-wrapper"
-                        style={{background: areDatesSame(new Date(), addDateBy(mondayDate, index)) ? "#F2CEE6" : ""}}
-                        onClick={() => onAddEvent(addDateBy(mondayDate, index))}
-                    >
-                        <p>{day}</p>
-                        {events.map((event) => areDatesSame(addDateBy(mondayDate, index), event.date) && (
-                            <div 
-                                className="event" 
-                                style={{height: `${event.howLong * HOUR_HEIGHT}px`, top: `${event.date.getHours() * eventHours + event.date.getMinutes() / 2}px`}}
-                            >
-                                Hi
-                            </div>
-                        ))}
-                    </div>
+                    <>
+                        <div 
+                            key={index} 
+                            className="day-wrapper"
+                            style={{background: areDatesSame(new Date(), addDateBy(mondayDate, index)) ? "#F2CEE6" : ""}}
+                            onClick={() => onAddEvent(addDateBy(mondayDate, index))}
+                        >
+                            <p>{day}</p>
+                            {events.map((event) => areDatesSame(addDateBy(mondayDate, index), event.date) && (
+                                <div 
+                                    key={event.howLong}
+                                    className="event" 
+                                    style={{height: `${event.howLong * HOUR_HEIGHT}px`, top: `${event.date.getHours() * eventHours + event.date.getMinutes() / 2}px`}}
+                                >
+                                    {event.text}
+                                </div>
+                            ))}
+                        </div>
+                        {/* {showModal && <Modal setShowModal={setShowModal} setFormData={setFormData} date={eventDate.toDateString()} />} */}
+                    </>
                 ))}
             </div>
         </div>
-        <div className="hour-line" style={{top: hourLine}}></div>
+        {!showModal && <div className="hour-line" style={{top: hourLine}}></div>}
       </div>
     </>
   );

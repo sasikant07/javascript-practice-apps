8ce4e081743fe381bcc2f255d77e39ddca37108a
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-25 01:30:13 +0530

    Tabs easy

---
+++ tabs-easy/src/App.js
@@ -0,0 +1,28 @@
+import Tabs from "./components/Tabs";
+
+const tabsData = [
+  {
+    label: "Profile",
+    content: <div>Profile Info Content</div>,
+  },
+  {
+    label: "Dashboard",
+    content: <div>Dashboard Content</div>,
+  },
+  {
+    label: "Settings",
+    content: <div>Settings Content</div>,
+  },
+  {
+    label: "Invoice",
+    content: <div>Invoice Content</div>,
+  },
+];
+
+export default function App() {
+  const onTabChangeHandler = (index) => {
+    console.log("Tab Changed");
+  };
+
+  return <Tabs tabsData={tabsData} onChange={onTabChangeHandler} />;
+}
---
+++ tabs-easy/src/components/Tabs.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./Tabs.css";
+
+export default function Tabs({ tabsData, onChange }) {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  return (
+    <div className="tabs">
+      <div className="tabs__container">
+        {tabsData.map((item, index) => {
+          return (
+            <button
+              key={index}
+              className={`${currentTabIndex === index ? "active" : ""}`}
+              onClick={() => {
+                setCurrentTabIndex(index);
+                onChange(index);
+              }}
+            >
+              {item.label}
+            </button>
+          );
+        })}
+      </div>
+      <div className="tabs__content">{tabsData[currentTabIndex].content}</div>
+    </div>
+  );
+}

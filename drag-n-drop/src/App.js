2117b13a29ccefb7f2df49552c88f3baddc6f030
Sasikant Biswal <biswalsashikant@gmail.com>
2025-03-02 20:04:17 +0530

    Drag n Drop

---
+++ drag-n-drop/src/App.js
@@ -0,0 +1,20 @@
+import DragAndDrop from "./components/DragAndDrop";
+
+const initialData = {
+  Todo: [
+    "Design UI mockups",
+    "Set up project repository",
+    "Write unit test",
+    "Integrate payment gateway",
+  ],
+  "In Progress": ["Develop authentication flow", "Implement responsive design"],
+  Completed: [
+    "Set up CI/CD pipeline",
+    "Conduct code reviews",
+    "Deploy initial version to staging",
+  ],
+};
+
+export default function App() {
+  return <DragAndDrop initialState={initialData} />;
+}
---
+++ drag-n-drop/src/components/DragAndDrop.jsx
@@ -0,0 +1,63 @@
+import { useRef, useState } from "react";
+
+export default function DragAndDrop({ initialState }) {
+  const [data, setData] = useState(initialState);
+  const dragItem = useRef();
+  const dragContainer = useRef();
+
+  const handleDragStart = (e, item, container) => {
+    dragItem.current = item;
+    dragContainer.current = container;
+    e.target.style.opacity = "0.5";
+  };
+
+  const handleDragEnd = (e) => {
+    e.target.style.opacity = "1";
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, targetContainer) => {
+    const item = dragItem.current;
+    const sourceContainer = dragContainer.current;
+    setData((prev) => {
+      const newData = { ...prev };
+      newData[sourceContainer] = newData[sourceContainer].filter(
+        (i) => i !== item
+      );
+      newData[targetContainer] = [...newData[targetContainer], item];
+      return newData;
+    });
+  };
+  return (
+    <div className="container">
+      {Object.keys(data).map((container, index) => {
+        return (
+          <div
+            key={index}
+            onDrop={(e) => handleDrop(e, container)}
+            onDragOver={handleDragOver}
+            className="item-container"
+          >
+            <h2>{container}</h2>
+            {data[container].map((item, idx) => {
+              return (
+                <div
+                  key={idx}
+                  onDragStart={(e) => handleDragStart(e, item, container)}
+                  onDragEnd={handleDragEnd}
+                  draggable
+                  className="item"
+                >
+                  {item}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
